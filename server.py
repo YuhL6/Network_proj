82a14683647c2e8f674362bf8086f64d3e6b2e74
YuhL6 <49777375+YuhL6@users.noreply.github.com>
2019-12-31 02:54:57 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,48 +1,96 @@
+import sys
+import optparse
 import socket
+import select
+import errno
+import pytun
+import dns.message
+import dns.name
+import dns.query
+import dns.resolver
+import base64 as coder
+import dns.rrset
+from queue import Queue
+from queue import LifoQueue
+local_addr = '10.9.0.2'
+local_mask = '255.255.255.0'
+mtu = 180
+server_port = 53
 
 
-class server:
+class server_tun:
     def __init__(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.sock.bind(('', 53))
-        self.packet_id = {}
+        '''
+        IFF_NO_PI: no flag in discrete packet
+        '''
+        self._tun = pytun.TunTapDevice(name='mytun', flags=pytun.IFF_TUN| pytun.IFF_NO_PI)
 
-    def writePacket(self, data):
-        pass
+        self._tun.addr = local_addr
+        self._tun.netmask = local_mask
+        self._tun.mtu = mtu
+        self._tun.persist(True)
+        self._tun.up()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._socket.bind(('', 53))
+        self.data_queue = LifoQueue(65532)
+        self.ip_queue = Queue(65532)
 
-    def packetsRead(self):
-        packet = ''
+    def _recv_from_socket(self):
+        data, addr = self._socket.recvfrom(65523)
+        query_msg = dns.message.from_wire(data)
+        name = str(query_msg.question[0].name)
+        name = name[:-20]
+        ip_data = ''.join(name.split('.'))
+        data_to_tun = coder.b64decode(ip_data)
+
+        self.data_queue.put((query_msg, addr))
+
+        return data_to_tun
+
+    def run(self):
+
+        mtu = self._tun.mtu
+        r = [self._tun, self._socket]
+        w = []
+        x = []
+        data_to_tun = ''
+        data_to_socket = ''
+        target_addr = ()
+        query_msg = None
+
         while True:
-            data = self.sock.recv(1024).decode()
-            packet += data[1:]
-            if data[0] == 'x':
-                self.writePacket(data)
-            elif data[0] == 'y':
-                pass
+            # print(1)
+            r, w, x = select.select(r, w, x)
+            if self._tun in r:
+                data_to_socket = self._tun.read(mtu)
+                print(data_to_socket)
+            if self._socket in r:
+                data_to_tun = self._recv_from_socket()
+                # print(data_to_tun)
+            if self._tun in w:
+                self._tun.write(data_to_tun)
+                data_to_tun = ''
+            if self._socket in w and not self.data_queue.empty():
+                query_msg, target_addr = self.data_queue.get()
+                response = dns.message.make_response(
+                    query_msg, recursion_available=True)
+                response.answer.append(dns.rrset.from_text(
+                    query_msg.question[0].name, 30000, 1, 'TXT', str(coder.b64encode(data_to_socket), encoding='ascii')))
+                self._socket.sendto(response.to_wire(), target_addr)
+                data_to_socket = b''
+            r = []
+            w = []
+            if data_to_tun:
+                w.append(self._tun)
+            else:
+                r.append(self._socket)
 
-    def cut_data(self, data):
-        if len(data) <= 300:
-            return [data]
-        else:
-            li = [data[0:300]]
-            data = data[300:]
-            while len(data) > 300:
-                li.append(data[0:300])
-                data = data[300:]
-            li.append(data)
-            return data
+            if data_to_socket:
+                w.append(self._socket)
+            else:
+                r.append(self._tun)
 
-    def packetSend(self, data):
-        data = data.decode()
-        li = self.cut_data(data)
-        for tmp in li:
-            self.udpSend(tmp)
-        self.count += 1
 
-    def udpSend(self, data):
-        prefix = '{} 01 20 00 0100 00 00 00 00 00 0d'.format(hex(self.count))
-        domain = '08 67 72 6f 75 70 2d 31 37 0563 73 33 30 35 03 66 75 6e 00 00 0a 00 01'
-        msg = prefix + data + domain
-        msg = msg.replace(' ', '')
-        msg = bytes.fromhex(msg)
-        self.sock.sendto(msg, ('120.78.166.34', 53))
+if __name__ == '__main__':
+    server = server_tun()
+    server.run()
